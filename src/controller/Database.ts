97599d77e9a1dc4a6fb621ec34ffb4cb74966677
romanwtk <roman.wetenkamp@gmx.de>
2022-11-06 11:50:54 +0100

    added database support (not working now, needs to be fixed with team!)

---
+++ src/controller/Database.ts
@@ -0,0 +1,31 @@
+import * as dotenv from "dotenv"
+dotenv.config()
+import * as mongodb from "mongodb";
+import { Filter } from "mongodb";
+
+const uri = String(process.env.DB_CONN_STRING);
+let client: mongodb.MongoClient;
+
+export async function connectDB() {
+  client = await mongodb.MongoClient.connect(String(process.env.DB_CONN_STRING));
+  console.log(uri);
+  // Use connect method to connect to the server
+  console.log("")
+  return "done.";
+}
+export async function closeDB() {
+  await client.close();
+  return "connection closed."
+}
+export async function getDataDB(dbName: string, collection: string) {
+  return await client.db(dbName).collection(collection).findOne();
+}
+
+export async function sendDataDB(dbName: string, collection: string, payload: object) {
+  return await client.db(dbName).collection(collection).insertOne(payload);
+}
+
+export async function deleteDataDB(dbName: string, collection: string, filter: Filter<object>) {
+  return await client.db(dbName).collection(collection).deleteOne(filter);
+}
+
---
+++ src/controller/PDataController.ts
@@ -0,0 +1,29 @@
+import { Controller, Example, Get, Route } from "tsoa";
+import { Patient } from "../model/PData";
+import { closeDB, connectDB, getDataDB } from "./Database";
+
+
+@Route("data")
+export class PDataController extends Controller {
+  @Example<Patient>({
+                      id: "15d37d7a-bd45-49b4-b83c-bd3393c2ca91",
+                      first_name: "Gernot",
+                      last_name: "Hassknecht",
+                      street: "Ehrenfelder Straße",
+                      number: 7,
+                      postcode: 516915,
+                      city: "Köln",
+                      insurance: "BARMER",
+                      insurance_number: "N26815181181585138",
+                 })
+  @Get("")
+  public async getData() {
+    connectDB().then((result) => {
+      console.log(result)
+      getDataDB("DoctorDocs", "patients").then(
+        (result) => console.log(result)
+      )
+      closeDB()
+    })
+  }
+}
