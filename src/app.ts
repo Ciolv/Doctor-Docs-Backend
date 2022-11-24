55ba3bd5bb813c7e268c4d245f30ed9e58c12a16
Ciolv <73327363+Ciolv@users.noreply.github.com>
2022-11-24 20:16:15 +0100

    Add CORS

--- src/app.ts
+++ src/app.ts
@@ -2,15 +2,23 @@
 import express, { json, Request, Response, urlencoded } from "express";
 import { RegisterRoutes } from "../build/routes";
 import swaggerUi from "swagger-ui-express";
+import cors from "cors";
 
 export const app = express();
 
 // Use body parser to read sent json payloads
 app.use(
   urlencoded({
-    extended: true,
-  })
+               extended: true
+             })
 );
+
+const corsOptions = {
+  origin: "http://localhost:3000",
+  optionsSuccessStatus: 200
+};
+
+app.use(cors(corsOptions));
 app.use(json());
 
 app.use("/docs", swaggerUi.serve, async (_req: Request, res: Response) => {
