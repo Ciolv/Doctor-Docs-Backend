2d8df2acd1928743b34e4107cac8d09cfea813e8
romanwtk <roman.wetenkamp@gmx.de>
2022-12-06 19:44:42 +0100

    general user instead of patient/doc

--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -1,16 +1,18 @@
 import { Body, Controller, Example, Get, Path, Post, Route } from "tsoa";
-import { Patient } from "../model/Patient";
+import { User } from "../model/User";
 import { Database } from "./Database";
 import { DatabaseUser } from "../model/DatabaseUser";
 
 @Route("users")
-export class PatientController extends Controller {
+export class UserController extends Controller {
   readDatabaseHandler: Database = new Database(DatabaseUser.LEGET, "accounts", "users");
+  readDoctorDatabaseHandler: Database = new Database(DatabaseUser.LEGET, "accounts", "doctors");
   writeDatabaseHandler: Database = new Database(DatabaseUser.SCRIBIT, "accounts", "users");
+  writeDoctorDatabaseHandler: Database = new Database(DatabaseUser.SCRIBIT, "accounts", "doctors");
   // deleteDatabaseHandler: Database = new Database(DatabaseUser.EXSTINGUET, "accounts", "users");
   updateDatabaseHandler: Database = new Database(DatabaseUser.REPONIT, "accounts", "users");
 
-  @Example<Patient>({
+  @Example<User>({
     id: "15d37d7a-bd45-49b4-b83c-bd3393c2ca91",
     first_name: "Gernot",
     last_name: "Hassknecht",
@@ -20,17 +22,25 @@ export class PatientController extends Controller {
     city: "Köln",
     insurance: "BARMER",
     insurance_number: "N26815181181585138",
+    approbation: "Regierungspräsidium Stuttgart",
   })
   @Get("{userId}")
   public async getData(@Path() userId: string) {
-    return await this.readDatabaseHandler.getUser(userId);
+    const user = await this.readDatabaseHandler.getUser(userId);
+    if (user !== null) {
+      return user;
+    }
+    return await this.readDoctorDatabaseHandler.getUser(userId);
   }
 
   @Get("registrationCompleted/{userId}")
   public async userRegistrationCompleted(@Path() userId: string) {
-    const user = await this.readDatabaseHandler.getUser(userId);
+    let user = await this.readDatabaseHandler.getUser(userId);
     if (user === null) {
-      return { completed: false };
+      user = await this.readDoctorDatabaseHandler.getUser(userId);
+      if (user === null) {
+        return { completed: false };
+      }
     }
 
     const allFieldsSet =
@@ -41,9 +51,8 @@ export class PatientController extends Controller {
       user.last_name !== null &&
       user.last_name !== "" &&
       user.insurance !== null &&
-      user.insurance !== "" &&
-      user.insurance_number !== null &&
-      user.insurance_number !== "" &&
+      ((user.insurance !== "" && user.insurance_number !== null && user.insurance_number !== "") ||
+        user.approbation !== "") &&
       user.postcode !== null &&
       user.street !== null &&
       user.street !== "" &&
@@ -59,7 +68,10 @@ export class PatientController extends Controller {
   }
 
   @Post("registration")
-  public async userRegistration(@Body() requestBody: Patient) {
-    return await this.writeDatabaseHandler.updateUser(requestBody);
+  public async userRegistration(@Body() requestBody: User) {
+    if (requestBody.approbation === "") {
+      return await this.writeDatabaseHandler.updateUser(requestBody);
+    }
+    return await this.writeDoctorDatabaseHandler.updateUser(requestBody);
   }
 }
--- src/model/User.ts
+++ src/model/User.ts
@@ -1,13 +1,15 @@
-export class Patient {
-  id = "";
+export class User {
+  id: string;
   first_name: string;
   last_name: string;
   street: string;
   number: number;
   postcode: number;
   city: string;
-  insurance_number: string;
-  insurance: string;
+  insurance_number?: string;
+  insurance?: string;
+  approbation?: string;
+  verified? = false;
 
   constructor(
     first_name: string,
@@ -16,9 +18,17 @@ export class Patient {
     number: number,
     postcode: number,
     city: string,
-    insurance_number: string,
-    insurance: string
+    insurance_number?: string,
+    insurance?: string,
+    approbation?: string,
+    verified?: boolean,
+    id?: string
   ) {
+    if (id) {
+      this.id = id;
+    } else {
+      this.id = "";
+    }
     this.first_name = first_name;
     this.last_name = last_name;
     this.street = street;
@@ -27,5 +37,7 @@ export class Patient {
     this.city = city;
     this.insurance_number = insurance_number;
     this.insurance = insurance;
+    this.approbation = approbation;
+    this.verified = verified;
   }
 }
