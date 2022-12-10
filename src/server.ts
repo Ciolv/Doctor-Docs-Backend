1dcfdac867685851d8c93344377b095f4ae88630
romanwtk <roman.wetenkamp@gmx.de>
2022-12-10 16:22:13 +0100

    Added TLS support. Need to add a directory "security" and both key.pem / cert.pem!

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,11 @@
 import { app } from "./app";
+import * as https from "https";
+import * as fs from "fs";
 
 const port = process.env.PORT || 8080;
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
+https.createServer({key: fs.readFileSync("security/key.pem"), cert: fs.readFileSync("security/cert.pem")}, app).listen(port, () => {
+  console.log(`DoctorDocs-Backend listening at http://localhost:${port}`)
+})
+
+// app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
